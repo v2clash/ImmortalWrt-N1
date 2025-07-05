4c67ab98ad9500b034c97c4468b5617f20ab24e4
v2clash <155278998+v2clash@users.noreply.github.com>
2025-07-05 23:39:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
 # 修改IP
 sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
