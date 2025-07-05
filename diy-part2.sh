3ad585be679e03bc4aa620b413b114fc0e3e644c
v2clash <155278998+v2clash@users.noreply.github.com>
2025-07-05 23:36:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,5 +3,8 @@
 # 修改IP
 sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
 
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's/root:::0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
