6ecaaba0106f8f4b0ab02ed6974d6abe2bad596e
v2clash <155278998+v2clash@users.noreply.github.com>
2025-07-06 01:49:42 +0800

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -10,6 +10,10 @@ sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generat
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's/root:::0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
+# 修改版本号
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
+
 # 修改时区
 sed -i "s/timezone='UTC'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "/timezone='CST-8'/a\\\t\t\tset system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
@@ -17,4 +21,6 @@ sed -i "/timezone='CST-8'/a\\\t\t\tset system.@system[-1].zonename='Asia/Shangha
 # 修改默认主题（如果存在）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-echo "DIY-part2.sh 脚本执行完成" 
+# 添加晶晨宝盒
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
