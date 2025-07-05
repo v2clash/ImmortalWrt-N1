95f615fb7ce2cc0d8883ca79359218bdf74f589e
v2clash <155278998+v2clash@users.noreply.github.com>
2025-07-05 15:30:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,8 +2,6 @@
 
 # 修改IP
 sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
-# 修改主机名
-#sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
